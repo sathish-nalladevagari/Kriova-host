7c19d5e5309efecdf38eea7e8089433d68115f27
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2022-08-12 01:46:10 +0530

    third commit

--- src/components/register.js
+++ src/components/register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import CollapsibleExample from './nav';
+import Nav from './nav';
+import axios from 'axios'
 
 
 function Register (){
@@ -12,36 +13,60 @@ function Register (){
 
 
     function submitform(e){
-        e.preventDefault()
-        console.log(fname,lname,Username,email,password)
+        e.preventDefault();
+        console.log(fname,lname,Username,email,password);
+        setFname('')
+        setLname('')
+        setPassword('')
+        setUsername('')
+        setEmail('')
+        var payload = {
+            fname,lname,Username,email,password
+        }
+        console.log(payload)
+
+        axios({
+            url:"http://localhost:8080/save",
+            method:"POST",
+            data:payload,
+        })
+        .then(()=>{
+            console.log("sended")
+        }).catch((err)=>{
+            if (err) throw err;
+            console.log("not sended")
+        })
+
+        
+
       
 
     }
     return(
     <div className='register'>
-        <CollapsibleExample />
+        <Nav />
         
         <form onSubmit={submitform}>
             
             <h1>Register Here</h1><br/>
             <label>First Name</label><br/>
-            <input type="text" name="firstnamr" onChange={(e)=>{
+            <input type="text" name="firstnamr" value={fname} onChange={(e)=>{
                 setFname(e.target.value)
             }} /><br/>
             <label>Last Name</label><br/>
-            <input type="text" name="lastname" onChange={(e)=>{
+            <input type="text" name="lastname" value={lname} onChange={(e)=>{
                 setLname(e.target.value)
             }} /><br/>
             <label>Username</label><br/>
-            <input type="text" name="username" onChange={(e)=>{
+            <input type="text" name="username" value={Username} onChange={(e)=>{
                 setUsername(e.target.value)
             }} /><br/>
             <label>Email</label><br/>
-            <input type="email" name="email" onChange={(e)=>{
+            <input type="email" name="email" value={email} onChange={(e)=>{
                 setEmail(e.target.value)
             }} /><br/>
             <label>Password</label><br/>
-            <input type="password" name="password" onChange={(e)=>{
+            <input type="password" name="password" value={password} onChange={(e)=>{
                 setPassword(e.target.value)
             }} /><br/>
             <button type="submit">Sign Up</button><br/>
